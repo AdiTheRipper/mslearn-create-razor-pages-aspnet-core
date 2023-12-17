220c630b2c000462f400670917a8fdac9fce0e30
Aditya <105660945+AdiTheRipper@users.noreply.github.com>
2023-12-17 06:49:00 +0000

    Pending changes exported from your codespace

--- Pages/PizzaList.cshtml.cs
+++ Pages/PizzaList.cshtml.cs
@@ -1,12 +1,33 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+
 using ContosoPizza.Models;
 using ContosoPizza.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace ContosoPizza.Pages
 {
     public class PizzaListModel : PageModel
     {
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid || NewPizza == null)
+            {
+                return Page();
+            }
+            _service.AddPizza(NewPizza);
+            return RedirectToAction("Get");
+        }
+        public IActionResult OnPostDelete(int id)
+{
+    _service.DeletePizza(id);
+
+    return RedirectToAction("Get");
+}
+
+
         private readonly PizzaService _service;
+
+        [BindProperty]
+        public Pizza NewPizza { get; set; } = default!;
         public IList<Pizza> PizzaList { get;set; } = default!;
         public PizzaListModel(PizzaService service)
         {
